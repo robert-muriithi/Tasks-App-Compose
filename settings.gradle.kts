ac30a89e3e42bde5d07d5b14fda29b83caf0b09c
Robert Murithi <robertmurithi@Roberts-MacBook-Pro.local>
2024-05-29 13:22:31 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ComposeTODO"
+include(":app")
+include(":core")
+include(":feature")
+include(":feature:tasks")
+include(":feature:settings")
+include(":design-system")
+include(":core:database")
+include(":core:network")
