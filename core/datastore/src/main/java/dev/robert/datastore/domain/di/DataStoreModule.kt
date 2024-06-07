d9e1c66735a0a47fb6b6a6cd18b5230c4fac985d
robertmurithi <njuguiinirobert@gmail.cm>
2024-06-07 11:27:13 +0300

    provide data store prefs

--- core/datastore/src/main/java/dev/robert/datastore/domain/di/DataStoreModule.kt
+++ core/datastore/src/main/java/dev/robert/datastore/domain/di/DataStoreModule.kt
@@ -1,17 +1,40 @@
 package dev.robert.datastore.domain.di
 
+import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dev.robert.datastore.data.TodoAppPreferences
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
+    @[Provides Singleton]
+    internal fun providesDataStorePreferences(
+        @ApplicationContext appContext: Context
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = {
+                appContext.preferencesDataStoreFile("dev.robert.todoapp")
+            }
+        )
+    }
     @Provides
     @Singleton
     fun provideTodoAppPreferences(dataStore: DataStore<Preferences>) = TodoAppPreferences(dataStore)
