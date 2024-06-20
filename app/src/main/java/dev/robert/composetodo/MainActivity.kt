7f9767a088a2c5d23dbf50bf3893c035ef948f7d
robertmurithi <njuguiinirobert@gmail.cm>
2024-06-21 00:14:17 +0300

    setup launcher icons, splash screen and redirect on splash screen

--- app/src/main/java/dev/robert/composetodo/MainActivity.kt
+++ app/src/main/java/dev/robert/composetodo/MainActivity.kt
@@ -1,12 +1,15 @@
 package dev.robert.composetodo
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.robert.composetodo.navigation.TodoCoreNavigator
 import dev.robert.design_system.presentation.theme.Theme
@@ -14,17 +17,33 @@ import kotlinx.coroutines.Dispatchers
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.showSplashScreen.value
+            }
+        }
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
-            val viewModel: MainActivityViewModel = hiltViewModel()
             val theme by viewModel.currentTheme.collectAsState(
                 initial = Theme.FOLLOW_SYSTEM.themeValue,
                 context = Dispatchers.Main.immediate,
             )
+            val startDestination = viewModel.startDestination.value
             TodoCoreNavigator(
                 theme = theme,
+                startDestination = startDestination
             )
         }
     }
--- feature/auth/src/main/java/dev/robert/auth/presentation/components/TextFields.kt
+++
@@ -1,1 +0,0 @@
-package dev.robert.auth.presentation.components
