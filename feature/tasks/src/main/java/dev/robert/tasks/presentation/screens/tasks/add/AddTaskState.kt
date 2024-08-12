2345d5ac7b1d02b97bd716123a839183e0a5bcb6
robertmurithi <njuguiinirobert@gmail.cm>
2024-08-12 13:36:49 +0300

    work on add task ui logic flow

--- feature/tasks/src/main/java/dev/robert/tasks/presentation/screens/tasks/add/AddTaskState.kt
+++ feature/tasks/src/main/java/dev/robert/tasks/presentation/screens/tasks/add/AddTaskState.kt
@@ -1,6 +1,21 @@
 package dev.robert.tasks.presentation.screens.tasks.add
 
+import dev.robert.tasks.domain.model.TaskCategory
+
 data class AddTaskState(
     val isLoading: Boolean = false,
-    val error: String = ""
+    val error: String = "",
+    val taskTitle: String = "",
+    val taskTitleError: String? = null,
+    val taskDescription: String = "",
+    val taskDescriptionError: String? = null,
+    val taskStartDate: String = "",
+    val taskStartDateError: String? = null,
+    val taskEndDate: String = "",
+    val taskEndDateError: String? = null,
+    val taskPriority: String = "",
+    val category: TaskCategory? = null,
+    val categoryError: String? = null,
+    val startTime: String = "",
+    val endTime: String = "",
 )
