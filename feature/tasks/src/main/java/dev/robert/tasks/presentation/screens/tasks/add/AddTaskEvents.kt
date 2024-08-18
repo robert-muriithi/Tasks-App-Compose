a8c9636a162da77441ef622475b56ff5ef5995f2
robertmurithi <njuguiinirobert@gmail.cm>
2024-08-18 21:35:20 +0300

    add task events and ui state

--- feature/tasks/src/main/java/dev/robert/tasks/presentation/screens/tasks/add/AddTaskEvents.kt
+++ feature/tasks/src/main/java/dev/robert/tasks/presentation/screens/tasks/add/AddTaskEvents.kt
@@ -1,5 +1,10 @@
 package dev.robert.tasks.presentation.screens.tasks.add
 
+import dev.robert.tasks.domain.model.TaskCategory
+
 sealed class AddTaskEvents {
-    data object AddTask : AddTaskEvents()
+    data object CreateTaskEvent : AddTaskEvents()
+    data class SelectCategoryEvent(val category: TaskCategory) : AddTaskEvents()
+    data object GetCategoriesEvent : AddTaskEvents()
+    data object AddCategoryEvent : AddTaskEvents()
 }
