632d05f221633919fd142fa788e30fe32a01b1d6
robertmurithi <njuguiinirobert@gmail.cm>
2024-09-02 15:53:32 +0300

    setup spotless

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Robert Muriithi.
+ * Copyright $YEAR Robert Muriithi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.robert.onboarding.domain.model
-
-import androidx.annotation.DrawableRes
-
-data class OnboardingItem(
-    val title: String,
-    val description: String,
-    @DrawableRes val image: Int,
-)
